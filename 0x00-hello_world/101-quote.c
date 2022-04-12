59cb5c89389b9d06eb5539e1dfc0c43838241ee2
injifannoo <injifanotu@gmail.com>
2022-04-12 16:59:13 +0000

    My 8th commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#!/bin/bash
-#include <stddio.h>
+#include <stdio.h>
  /** * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+printf("and that piece of art is useful" - Dora Korbar, 2015-10-19);
 return (0);
-printf("and that piece of art is useful" - Dora Korbar, 2015-10-19)
 }
