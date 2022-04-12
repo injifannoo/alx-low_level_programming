e375ca07fbcb108d5fe2400a7438438f16336671
injifannoo <injifanotu@gmail.com>
2022-04-12 17:10:03 +0000

    My 8th commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
- /** * main - Entry point
+ /**
+ * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-printf("and that piece of art is useful - Dora Korbar, 2015-10-19\n");
+printf("and that piece of art is useful"" - Dora Korbar, 2015-10-19\n");
 return (0);
 }
