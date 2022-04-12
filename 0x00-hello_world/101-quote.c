e751aa843300684b37454226cedd57862c0b46fd
injifannoo <injifanotu@gmail.com>
2022-04-12 16:56:47 +0000

    My 8th commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stddio.h>
+ /** * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+return (0);
+printf("and that piece of art is useful" - Dora Korbar, 2015-10-19)
+}
