cb136bec3f9a3ff1803f2945a87e07f2302349ed
injifannoo <injifanotu@gmail.com>
2022-04-12 17:19:23 +0000

    My 8th commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-printf("and that piece of art is useful"" - Dora Korbar, 2015-10-19\n");
+printf("and that piece of art is useful\" - Dora Korbar, 2015-10-19\n");
 return (0);
 }
