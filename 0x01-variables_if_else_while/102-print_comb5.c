df4686bb205ca636900a37899c0b85d6899af8e0
injifannoo <injifanotu@gmail.com>
2022-04-16 03:20:40 -0700

    My commit message

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,21 +8,11 @@
 
 int main(void)
 
-
-
-
-
-
-
 {
 
 int first_nums;
 
 int second_nums;
-
-
-
-
 
 for (first_nums = 0; first_nums < 100 ; first_nums++)
 
@@ -31,28 +21,14 @@ for (first_nums = 0; first_nums < 100 ; first_nums++)
 for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
 
 {
-
-
-
 putchar (first_nums / 10 + '0');
 
 putchar (first_nums % 10 + '0');
 
-
-
-
-
 putchar (' ');
-
-
-
 putchar (second_nums / 10 + '0');
 
 putchar (second_nums % 10 + '0');
-
-
-
-
 
 if (first_nums == 98 && second_nums == 99)
 
@@ -62,34 +38,16 @@ break;
 
 }
 
-
-
-
-
 putchar(',');
 
 putchar (' ');
 
-
-
-
-
 }
 
-
-
 }
-
-
 
 putchar('\n');
 
-
-
-
-
 return (0);
-
-
 
 }
