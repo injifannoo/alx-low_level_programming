249b19ca34809e252e1e9bca1565974cfd538f30
injifannoo <injifanotu@gmail.com>
2022-04-16 03:17:33 -0700

    My commit message

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,11 @@
 
 int main(void)
 
-
-
 {
 
 int number_left;
 
 int number_right;
-
-
-
 
 
 for (number_left = 48; number_right <= 78; number_left++)
@@ -28,15 +23,9 @@ for (number_right = number_left + 1 ; number_right <= 78; number_right++)
 
 {
 
-
-
 putchar(number_left);
 
 putchar (number_right);
-
-
-
-
 
 if ((number_left == 56) && (number_right == 78))
 
@@ -46,32 +35,13 @@ break;
 
 }
 
-
-
 putchar(',');
 
 putchar (' ');
 
-
-
 }
 
-
-
 }
-
-
-
 putchar('\n');
-
-
-
-
-
 return (0);
-
-
-
-
-
 }
