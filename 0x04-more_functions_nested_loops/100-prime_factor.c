28d17e649a9b93b62555d83b8524f74164f8af0e
Injifannoo Urgeessaa <injifanotu@gmail.com>
2022-04-15 17:21:49 -0700

    My commit message

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,14 +3,14 @@
 
 
 /**
-
- * main - Entry point.
-
  *
-
- * Return: Always 0.
-
- */
+ *  * main - Entry point.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
 
 
 
@@ -20,28 +20,28 @@ int main(void)
 
 {
 
-long n, i;
+	long n, i;
 
 
 
-n = 612852475143;
+	n = 612852475143;
 
-for (i = 2; i < n; i++)
+	for (i = 2; i < n; i++)
 
-{
+	{
 
-while (n % i == 0)
+		while (n % i == 0)
 
-n = n / i;
+			n = n / i;
 
-}
-
-
-
-printf("%lu\n", n);
+	}
 
 
 
-return (0);
+	printf("%lu\n", n);
+
+
+
+	return (0);
 
 }
