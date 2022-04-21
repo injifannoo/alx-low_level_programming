7f947f0a8fd08bcce1dc7c0be5e0d3a639ff5a41
injifannoo <injifanotu@gmail.com>
2022-04-21 11:26:24 -0700

    My commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,18 +1,13 @@
 #include "main.h"
-
 #include <stdio.h>
-
 /**
  * isPrintableASCII - determines if n is a printable ASCII char
  * @n: integer
  * Return: 1 if true, 0 if false
  */
-
 int isPrintableASCII(int n)
-
 {
 	return (n >= 32 && n <= 126);
-
 }
 /**
  * printHexes - print hex values for string b in formatted form
@@ -20,39 +15,23 @@ int isPrintableASCII(int n)
  * @start: starting position
  * @end: ending position
  */
-
 void printHexes(char *b, int start, int end)
 
 {
-
 	int i = 0;
-
-
-
 	while (i < 10)
 
 	{
-
 		if (i < end)
 
-			printf("%02x", *(b + start + i));
-
+		printf("%02x", *(b + start + i));
 		else
-
 			printf("  ");
-
 		if (i % 2)
-
 			printf(" ");
-
 		i++;
-
 	}
-
 }
-
-
-
 /**
  * printASCII - print ascii values for string b,
  * formatted to replace nonprintable chars with '.'
@@ -63,18 +42,12 @@ void printHexes(char *b, int start, int end)
 void printASCII(char *b, int start, int end)
 {
 	int ch, i = 0;
-
 	while (i < end)
-
          {
 		ch = *(b + i + start);
-
 		if (!isPrintableASCII(ch))
-
 			ch = 46;
-
 		printf("%c", ch);
-
 		i++;
 	}
 }
@@ -84,7 +57,6 @@ void printASCII(char *b, int start, int end)
  * @b: string
  * @size: size of buffer
  */
-
 void print_buffer(char *b, int size)
 
 {
@@ -97,19 +69,13 @@ void print_buffer(char *b, int size)
 
 		{
 			end = (size - start < 10) ? size - start : 10;
-
 			printf("%08x: ", start);
-
 			printHexes(b, start, end);
-
 			printASCII(b, start, end);
-
 			printf("\n");
 
 		}
 
 	} else
-
 		printf("\n");
-
 }
