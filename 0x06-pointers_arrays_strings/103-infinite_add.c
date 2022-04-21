623cf8a89fdc93dab681011c0a22046f82f78846
injifannoo <injifanotu@gmail.com>
2022-04-21 10:59:52 -0700

    My commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,14 +10,9 @@
  * @size_r: buffer size:
  * Return: the pointer to dest.
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-
 {
-
 	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
-
-
 
 	while (*(n1 + c1) != '\0')
 
@@ -30,7 +25,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (c1 >= c2)
 
 		bg = c1;
-
 	else
 
 		bg = c2;
@@ -56,15 +50,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			add = op / 10;
 
 		else
-
-			add = 0;
+	add = 0;
 
 		if (op > 0)
 
 		*(r + bg) = (op % 10) + 48;
 
 		else
-
 			*(r + bg) = '0';
 
 		if (c1 > 0)
@@ -78,21 +70,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (c2 > 0)
 
 			c2--, dr2 = *(n2 + c2) - 48;
-
 		else
 
 			dr2 = 0;
-
 		bg--, size_r--;
-
 	}
 
 	if (*(r) == '0')
 
 		return (r + 1);
-
 	else
-
 		return (r);
 
 }
